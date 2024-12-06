75d6429d0675effdb00ebee65714389da5aba520
Nat-Wrobel <128699255+Nat-Wrobel@users.noreply.github.com>
2024-12-06 12:50:35 +0100

    Zadanie 7

---
+++ zadania/zadanie_7.py
@@ -0,0 +1,30 @@
+import os
+from qgis.core import QgsVectorLayer
+from qgis.core import QgsField
+from qgis.PyQt.QtCore import QVariant
+
+path = os.path.join(os.path.expanduser("~"),"Documents/adg/02")
+os.chdir(path)
+
+vector = QgsVectorLayer("powiaty.gpkg")
+
+# 7 Oblicz długość granic i dodaj jako nowy atrybut do warstwy.
+# rozpoczęcie trybu edycji warstwy
+vector.startEditing()
+
+# dodanie nowego atrybutu do tabeli
+new_field = [QgsField("dlugosc_granic_km", QVariant.Double)]
+vector.dataProvider().addAttributes(new_field)
+vector.updateFields()
+
+# indeks nowego atrybutu
+length_idx = vector.fields().indexOf("dlugosc_granic_km")
+
+# obliczenie powierzchni dla każdego obiektu
+for feature in vector.getFeatures():
+    length = feature.geometry().length()/1000
+    # wprowadzenie wartości do atrybutu obiektu
+    vector.changeAttributeValue(feature.id(), length_idx, length)
+
+# zapisanie zmian
+vector.commitChanges()
