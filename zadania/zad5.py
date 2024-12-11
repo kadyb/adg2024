588499671caf7cc26d220a4c8e0ad4553b3e0631
TotalNoname <128639532+TotalNoname@users.noreply.github.com>
2024-12-11 18:05:48 +0100

    zad5.py (poprawione - A.Żywień)

--- zadania/zad5.py
+++ zadania/zad5.py
@@ -1,25 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec  6 11:56:17 2024
-"""
-#%% zadanie 5 (zadanie 1 z ćwiczenia 2)
+# poprawiona wersja 
 
-from qgis.core import QgsRasterLayer
+import qgis.core as qc
 
-dem = QgsRasterLayer('dane/DEM.tif')
+dem = qc.QgsRasterLayer('dane/DEM.tif')
 dem.isValid()
 
 def cell_size(dem):
-    xsize = dem.width()
-    ysize = dem.height()
-    ext = dem.extent().toString() # zwraca zasięg w postaci str
-    ext = ext.replace(' : ' ,',') # poprawki 
-    ext = ext.split(',') # rozdzielenie wartości
-    ext = [float(ext[i]) for i in range(len(ext))] # z str to float
-    cell_xsize = abs((ext[2]-ext[0])/xsize)
-    cell_ysize = abs((ext[3]-ext[1])/ysize)
+    cell_xsize = abs((dem.extent().width())/dem.width())
+    cell_ysize = abs((dem.extent().height())/dem.height())
     
     return [cell_xsize, cell_ysize]
 
 cell_size(dem)
+
