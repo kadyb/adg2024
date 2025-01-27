59a06f7c0bb55887f38831c75d044a79b04be8b8
Adam Olejniczak <128734923+adamolejniczak@users.noreply.github.com>
2025-01-27 23:21:51 +0100

    zadanie 8

---
+++ zadania/zadanie_8.py
@@ -0,0 +1,20 @@
+import statistics as stat
+import qgis.core
+
+path = os.path.join(os.path.expanduser("~"), "Documents", "dane")
+os.chdir(path)
+
+file = "powiaty.gpkg"
+vector = QgsVectorLayer(file)
+
+def powiaty_stats(vector):
+    ls = []
+    areas = []
+    for f in vector.getFeatures():
+        ls.append(f.attribute("dl_granicy"))
+    for f in vector.getFeatures():
+        areas.append(f.attribute("pole_km2"))
+    print("Statistics for border length:\nmean: {}\nmedian: {}\nmin: {}\nmax: {}\nstandard deviation: {}".format(stat.mean(ls), stat.median(ls), min(ls), max(ls), stat.stdev(ls)))
+    print("Statistics for area:\nmean: {}\nmedian: {}\nmin: {}\nmax: {}\nstandard deviation: {}".format(stat.mean(areas), stat.median(areas), min(areas), max(areas), stat.stdev(areas)))
+
+powiaty_stats(vector)
