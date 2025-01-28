b52093ee39849420e80ebbd38da813ad55784ba2
TotalNoname <128639532+TotalNoname@users.noreply.github.com>
2025-01-28 19:27:35 +0100

    Update zad16.py (Żywień)
    poprawione zadanie

--- zadania/zad16.py
+++ zadania/zad16.py
@@ -2,30 +2,36 @@ import os
 import qgis.core as qc
 from math import sqrt
 
-def SurfaceDistance(v,a,b):
-    f1 = csv_file.getFeature(a)
-    f2 = csv_file.getFeature(b)
-    
-    crs = qc.QgsCoordinateReferenceSystem("EPSG:2180")
-    dest_crs = qc.QgsCoordinateReferenceSystem("EPSG:2177")
-    trans_context = qc.QgsCoordinateTransformContext()
-    transform = qc.QgsCoordinateTransform(crs, dest_crs, trans_context)
-    
-    pt1 = qc.QgsVector3D(round(float(f1[1]),2), round(float(f1[2]),2), round(float(f1[3]),2))
-    pt2 = qc.QgsVector3D(round(float(f2[1]),2), round(float(f2[2]),2), round(float(f2[3]),2))
-    pt1 = transform.transform(pt1)
-    pt2 = transform.transform(pt2)
-    
-    return round(sqrt(abs(
-            pow(pt2.x() - pt1.x(),2) +
-            pow(pt2.y() - pt1.y(),2) + 
-            pow(pt2.z() - pt1.z(),2)
-            )),2)
+def SurfaceDistance(v):
+    if not v.isValid():
+        raise Exception('Nieprawidłowy plik')
+    sum_dist = 0
+    for f in range(1,v.featureCount()-1):
+        f1 = csv_file.getFeature(f)
+        f2 = csv_file.getFeature(f+1)
+        
+        crs = qc.QgsCoordinateReferenceSystem("EPSG:2180")
+        dest_crs = qc.QgsCoordinateReferenceSystem("EPSG:2177")
+        trans_context = qc.QgsCoordinateTransformContext()
+        transform = qc.QgsCoordinateTransform(crs, dest_crs, trans_context)
+        
+        pt1 = qc.QgsVector3D(float(f1[1]), float(f1[2]), float(f1[3]))
+        pt2 = qc.QgsVector3D(float(f2[1]), float(f2[2]), float(f2[3]))
+        pt1 = transform.transform(pt1)
+        pt2 = transform.transform(pt2)
+        
+        sum_dist += sqrt(
+                pow(pt2.x() - pt1.x(),2) +
+                pow(pt2.y() - pt1.y(),2) + 
+                pow(pt2.z() - pt1.z(),2)
+                )
+        
+    return sum_dist
     
 path = os.path.join(os.path.expanduser('~'),'algorytmy_geo')
 os.chdir(path)
+
 csv_path = os.path.join(path, 'dane','profil.csv')
 csv_file = qc.QgsVectorLayer(csv_path)
 
-dist = SurfaceDistance(csv_file,1,2) # 515.79 m jeżeli zastosowany zostanie epsg:2180
-dist2 = SurfaceDistance(csv_file,21,37)
+dist = SurfaceDistance(csv_file)
